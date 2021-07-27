e9c12b929409fb0530c1813772bf552a774fbf29
Jeremy Evans <jeremy.scott.evans@gmail.com>
2021-07-26 19:09:51 -0700

    Update settings.local.php

--- .tugboat/settings.local.php
+++ .tugboat/settings.local.php
@@ -8,7 +8,7 @@ $databases['default']['default'] = array (
   'port' => '3306',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
-  'isolation' => "SET SESSION tx_isolation='READ-COMMITTED'"
+  'isolation' => "SET SESSION tx_isolation='READ-COMMITTED'",
 );
 // Use the TUGBOAT_REPO_ID to generate a hash salt for Tugboat sites.
 $settings['hash_salt'] = hash('sha256', getenv('TUGBOAT_REPO_ID'));
